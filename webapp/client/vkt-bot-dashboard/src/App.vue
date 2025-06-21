98f7a9fa9ba18003732914fb813ec6c58f66ef0e
Max Rokitsky <max@rokitsky.ru>
2025-06-22 01:11:44 +0300

    feat(webapp): :tada: started a webapp development

---
+++ webapp/client/vkt-bot-dashboard/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useColorMode } from '@vueuse/core';
+import { RouterView } from 'vue-router'
+
+
+const mode = useColorMode()
+</script>
+
+<template>
+  <RouterView />
+</template>
